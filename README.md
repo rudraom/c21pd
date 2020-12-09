088f158beda53835fac1fd199f89af147dc6183a
rudraom <68151896+rudraom@users.noreply.github.com>
2020-12-08 22:09:45 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# c21pd
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,35 @@
+var bullet,wall,speed,weight,thickness;
+function setup() {
+  createCanvas(1600,400);
+  thickness=random(22,83)
+  speed=random(223,321)
+  weight=random(30,52)
+  car=createSprite(50, 200, 50, 50);
+  wall=createSprite(1200, 200, thickness, height/2);
+  
+  car.velocityX=speed;
+}
+
+function draw() {
+  background(5);
+  hascollided(lbullet,lwall);
+  if (hascollided(bullet,wall)){
+  bullet.velocityX=0;
+  var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+  if (damage>10){
+  wall.shapeColor=color(255,0,0)
+  }
+  if(damage<10){
+  wall.shapeColor=color(0,255,0);
+  }
+  }
+  drawSprites();
+}
+function hascollided(lbullet,lwall){
+bulletRightEdge=lbullet.x+lbullet.weight;
+wallLeftEdge=lwall.x;
+if(bulletRightEdge>=wallLeftEdge){
+return true
+}
+return false;
+}
